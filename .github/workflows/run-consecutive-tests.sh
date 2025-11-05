831b303d322f2e4c3a2e8d7b1d60fc5ab507389e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2025-11-05 12:18:14 +0100

    Distinguish failure types

--- .github/workflows/run-consecutive-tests.sh
+++ .github/workflows/run-consecutive-tests.sh
@@ -20,11 +20,21 @@ VERSIONS_JSON="${VERSIONS_JSON%"${VERSIONS_JSON##*[!\']}"}"
 readarray -t VERSIONS < <(echo "$VERSIONS_JSON" | jq -r '.[]')
 
 for VERSION in "${VERSIONS[@]}"; do
+  # check if javac works for the new version
+  GVM_TCK_LV="$VERSION" ./gradlew clean javac -Pcoordinates="$TEST_COORDINATES"
+  RESULT=$?
+  if [ "$RESULT" -eq 0 ]; then
+    echo "PASSED [javac]:$VERSION"
+  else
+    echo "FAILED [javac]:$VERSION"
+    break
+  fi
+
   echo "Running test with GVM_TCK_LV=$VERSION and coordinates=$TEST_COORDINATES"
   GVM_TCK_LV="$VERSION" ./gradlew test -Pcoordinates="$TEST_COORDINATES"
   RESULT=$?
-
   ATTEMPTS=1
+
   # maybe we failed because the test was flaky => try two more times to be sure
   while [ "$RESULT" -ne 0 ] && [ $ATTEMPTS -le 2 ]; do
     echo "Re-running the test with GVM_TCK_LV=$VERSION and coordinates=$TEST_COORDINATES"
@@ -34,9 +44,9 @@ for VERSION in "${VERSIONS[@]}"; do
   done
 
   if [ "$RESULT" -eq 0 ]; then
-    echo "PASSED:$VERSION"
+    echo "PASSED [native test]:$VERSION"
   else
-    echo "FAILED:$VERSION"
+    echo "FAILED [native test]:$VERSION"
     break
   fi
 done
